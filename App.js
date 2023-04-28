3d09ea534721c049088b8b2c70f2c15f6a6847c7
yustafk <106214324+yustafkusuma@users.noreply.github.com>
2023-04-29 06:49:36 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,22 +1,29 @@
-import React, {Component} from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Home from "./components/Home";
-import Result from "./components/Result";
+
+import Home from "./src/Home";
+import Detail from "./src/Detail";
+import UserPage from "./src/UserPage";
+import { Provider } from "react-redux";
+import store from "./src/redux-thunk/store";
 
 const Stack = createNativeStackNavigator();
 
-export default class App extends Component {
-  render() {
-    return (
+export default function App() {
+  return (
+    <Provider store={store}>
       <NavigationContainer>
         <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{headerShown: false}}>
-          <Stack.Screen name="Home" component={Home}/>
-          <Stack.Screen name="Result" component={Result}/>
+          initialRouteName="Home"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Detail" component={Detail} />
+          <Stack.Screen name="UserPage" component={UserPage} />
         </Stack.Navigator>
       </NavigationContainer>
-    );
-  }
+    </Provider>
+  );
 }
